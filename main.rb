7d8205fd4b1664a03cbadb3e3e040688817e6fa9
GroupBAudi <144666972+GroupBAudi@users.noreply.github.com.>
2024-09-06 18:38:39 +0700

    Rewrite the code as class

--- main.rb
+++ main.rb
@@ -15,96 +15,69 @@ class PlayerTwo < Player
   end
 end
 
-# Generate Board to play
-Board.generate
-
-def win_tiles
-  tiles = [
-  [[1,1], [1,2], [1,3]],
-  [[2,1], [2,2], [2,3]],
-  [[3,1], [3,2], [3,3]],
-
-  [[1,1], [2,1], [3,1]],
-  [[2,1], [2,2], [2,3]],
-  [[1,3], [2,3], [3,3]],
-  
-  [[1,1], [2,2], [3,3]],
-  [[1,3], [2,2], [3,1]]
-]
-end
-
-def check_has_tiles?(win=win_tiles, player)
-  win.each do |x|
-    correct_tiles = 0
-    player.tiles.each do |y|
-      correct_tiles+=1 if x.include?(y)
-      return true if correct_tiles == 3
-    end
+class Game
+  attr_reader :player_one, :player_two
+  attr_accessor :win, :tiles
+  def initialize(p_one, p_two)
+    @player_one = p_one
+    @player_two = p_two
+    @win = false
+    @tiles = []
+    @turn = false
   end
-end
 
-def game
-  player_one = PlayerOne.new('Player One')
-  player_two = PlayerTwo.new('Player Two')
+  def add_tiles(player, row, col)
+    player.add_mark(row, col)
+    player.tiles.push([row, col])
+  end
 
-  tiles = []
-  win = false
-  turn = false
-
-  while win != true
-    puts "It's #{player_one.name}'s turn now!" if turn == false
-    puts "It's #{player_two.name}'s turn now!" if turn == true
-    Board.print
-    puts 'Which row? '
-    row =  gets.chomp.to_i
-    puts  'Which column?'
-    col =  gets.chomp.to_i
-
-    if (row < 4 && row > 0) && (col < 4 && col > 0)
-      if turn == false
-        player_one.add_mark(row, col)
-        player_one.tiles.push([row, col])
-        check_has_tiles?(player_one) if player_one.tiles.length >= 3
-        win = true if check_has_tiles?(player_one) == true
-        puts "#{player_one.name} wins!" if check_has_tiles?(player_one) == true
-        turn = true
-      else
-        player_two.add_mark(row, col)
-        player_two.tiles.push([row, col])
-        check_has_tiles?(player_two) if player_two.tiles.length >= 3
-        win = true if check_has_tiles?(player_two) == true
-        puts "#{player_two.name} wins!" if check_has_tiles?(player_two) == true
-        turn = false
+  def check_has_tiles?(win=Board.win_tiles, player)
+    win.each do |x|
+      correct_tiles = 0
+      player.tiles.each do |y|
+        correct_tiles+=1 if x.include?(y)
+        return true if correct_tiles == 3
       end
-    else
-      puts "Can't enter "
     end
-    tiles.push(row, col)
+  end
+
+  def start_game
+    Board.generate
+    while !self.win
+      puts "It's #{player_one.name}'s turn now!" if @turn == false
+      puts "It's #{player_two.name}'s turn now!" if @turn == true
+      Board.print
+      puts 'Which row? '
+      row =  gets.chomp.to_i
+      puts  'Which column?'
+      col =  gets.chomp.to_i
+      if (row < 4 && row > 0) && (col < 4 && col > 0)
+        if !@turn
+          # add tiles
+          # check if he win
+          add_tiles(player_one, row, col)
+          @turn = true
+        else
+          # add tiles
+          add_tiles(player_two, row, col)
+          @turn = false
+        end
+      end
+      if player_one.tiles.length >= 3 || player_two.tiles.length >= 3
+        puts "#{player_one.name} wins" if check_has_tiles?(player_one) == true
+        puts "#{player_two.name} wins" if check_has_tiles?(player_two) == true
+        self.win = true if check_has_tiles?(player_one) == true || check_has_tiles?(player_two) == true
+      end
+    end
+  end
+
+  def test
+    p Board.win_tiles[0]
   end
 end
 
-game
+player_one = PlayerOne.new("Ambatukam")
+player_two = PlayerTwo.new("Omaygot")
 
-def test(win=win_tiles, arr)
-  win.each do |x|
-    correct_tiles = 0
-    arr.each do |y|
-      correct_tiles+=1 if x.include?(y)
-      correct_tiles
-      return true if correct_tiles == 3
-    end
-  end
-end
-
-arr = [[1,1],[2,2],[3,3]]
-
-# p test(arr)
-
-                                                                                                                                                                                                                                                                                                                                          
-
-def test2
-  arr = [[1,2], [1,3], [1,2]]
-  hash = arr.tally
-  hash.each  { |key, value| arr.uniq! if value > 1 }
-  p arr
-end
+game = Game.new(player_one, player_two)
+game.start_game
