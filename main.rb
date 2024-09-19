847ba4542f5c4e0d8d4e659b49ce2814d9cb8732
GroupBAudi <144666972+GroupBAudi@users.noreply.github.com.>
2024-09-19 07:14:48 +0700

    Fix score bug

--- main.rb
+++ main.rb
@@ -30,7 +30,7 @@ class Game
 
   def take_tile(player, number)
     if @tiles.include?(number)
-      puts "Tile's taken. Take another tile."
+      puts Rainbow("Tile's taken. Take another tile.").red
     else
       player.add_mark(number)
       @tiles.push(number)
@@ -53,10 +53,12 @@ class Game
     @tiles = []
     player_one.clear_taken_tiles
     player_two.clear_taken_tiles
-    @turn = false
+    player_one.correct_tiles = 0
+    player_two.correct_tiles = 0
+    @turn = false # change the turn back to player one
   end
 
-  def restart_game
+  def add_score
     if player_one.win == true
       player_one.score += 1
       player_one.win = false
@@ -64,42 +66,58 @@ class Game
       player_two.score += 1
       player_two.win = false
     end
-    puts "#{player_one.name}'s score = #{player_one.score}"
-    puts "#{player_two.name}'s score = #{player_two.score}"
+  end
+
+  def restart_game
+    add_score
+    puts "#{Rainbow(player_one.name).red}'s score = #{player_one.score}"
+    puts "#{Rainbow(player_two.name).blue}'s score = #{player_two.score}"
     reset_board
     start_game
+  end
+
+  def exit_game
+    add_score
+    puts "Thank you for playing!"
+    puts "#{Rainbow(player_one.name).red}'s score = #{player_one.score}"
+    puts "#{Rainbow(player_two.name).blue}'s score = #{player_two.score}"
   end
 
   def start_game
     Board.generate
     until player_one.win || player_two.win
-      puts "It's #{player_one.name}'s turn now!" if @turn == false
-      puts "It's #{player_two.name}'s turn now!" if @turn == true
+      puts "It's #{Rainbow(player_one.name).red}'s turn now!" if @turn == false
+      puts "It's #{Rainbow(player_two.name).blue}'s turn now!" if @turn == true
       Board.print_self
       puts 'Which tile?'
       i = gets.chomp.to_i
-      if !@turn
-        take_tile(player_one, i)
+      if i > 9 || i <= 0
+        puts "Oops! Out of range."
       else
-        take_tile(player_two, i)
+        if !@turn
+          take_tile(player_one, i)
+        else
+          take_tile(player_two, i)
+        end
       end
-      
       check_has_tiles?(player_one)
       check_has_tiles?(player_two) 
    
       puts "#{player_one.name} wins!" if player_one.win == true
       puts "#{player_two.name} wins!" if player_two.win == true
-      if self.tiles.length > 8 && (!player_one.win || !player_two.win) 
-        puts "Draw!"
-        reset_board
+
+      if self.tiles.length == 9 && (!player_one.win && !player_two.win) 
+        Board.print_self
+        puts "No possible tiles left! Restart the game? (y/n)"
+        reset_board_prompt = gets.chomp
+        reset_board if reset_board_prompt == "y"
         Board.generate
       end
     end
-    puts "Game has ended. Retry? (y/n lowercase only)"
-    reset_prompt = gets.chomp
-    restart_game if reset_prompt == "y"
+    puts "Game has ended! Restart? (y/n lowercase only)"
+    restart_game_prompt = gets.chomp
+    if restart_game_prompt == "y" then restart_game else exit_game end
   end
-  
 end
 
 player_one = PlayerOne.new('Player One')
